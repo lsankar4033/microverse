c73330c256a2d14c9d52274bda428b225b4892bf
lsankar4033 <lsankar4033@gmail.com>
2018-10-11 20:43:13 -0700

    [contract] Add Dutch Auction stage

--- eth/contracts/Microverse.sol
+++ eth/contracts/Microverse.sol
@@ -10,14 +10,107 @@ import "./TaxRules.sol";
 
 // TODO: Make HexBoard4
 contract Microverse is HexBoard2, PullPayment, Ownable, TaxRules, JackpotRules {
-
   using SafeMath for uint256;
 
-  mapping(uint8 => address) public tileToOwner;
+  // states this contract progresses through
+  enum Stage {
+    DutchAuction,
+    GameRounds
+  }
+  Stage public stage = Stage.DutchAuction;
 
-  // TODO: Ok to initialize these to 0?
+  modifier atStage(Stage _stage) {
+    require(
+      stage == _stage,
+      "Function cannot be called at this time."
+    );
+    _;
+  }
+
+  constructor() public {
+    _startAuction();
+  }
+
+  mapping(uint8 => address) public tileToOwner;
   mapping(uint8 => uint256) public tileToPrice;
   uint256 public totalTileValue;
+
+  // TODO: Change this once we define 'team'
+  function _sendToTeam(uint256 amount) private {
+    asyncSend(owner, amount);
+  }
+
+  function _changeTilePrice(uint8 tileId, uint256 newPrice) private {
+    uint256 oldPrice = tileToPrice[tileId];
+    tileToPrice[tileId] = newPrice;
+    totalTileValue = totalTileValue.add(newPrice.sub(oldPrice));
+  }
+
+  //////////
+  // Auction
+  //////////
+
+  uint256 constant public startingAuctionPrice = 1 ether;
+  uint256 constant public endingAuctionPrice = 0.1 ether;
+
+  uint256 constant public auctionDuration = 7 days; // period over which land price decreases linearly
+
+  uint256 public auctionStartTime;
+
+  function buyTileAuction(uint8 tileId, uint256 newPrice) public payable atStage(Stage.DutchAuction) {
+    // tile must not already have been bought
+    require(tileToOwner[tileId] == address(0) && tileToOwner[tileId] == 0);
+
+    uint256 tax = _priceToTax(newPrice);
+    uint256 price = getTilePriceAuction();
+
+    // must be paying the full price!
+    require(msg.value >= tax + price);
+
+    // NOTE: Team gets all proceeds of auction. Alternatively, this could be allocated to jackpot
+    _sendToTeam(tax.add(price));
+
+    tileToOwner[tileId] = msg.sender;
+    _changeTilePrice(tileId, newPrice);
+  }
+
+  function getTilePriceAuction() public view atStage(Stage.DutchAuction) returns (uint256) {
+    uint256 secondsPassed = 0;
+
+    // This should always be the case...
+    if (now > auctionStartTime) {
+      secondsPassed = now.sub(auctionStartTime);
+    }
+
+    if (secondsPassed >= auctionDuration) {
+      return endingAuctionPrice;
+    } else {
+      uint256 maxPriceDelta = startingAuctionPrice.sub(endingAuctionPrice);
+      uint256 actualPriceDelta = (maxPriceDelta.mul(secondsPassed)).div(auctionDuration);
+
+      return startingAuctionPrice.sub(actualPriceDelta);
+    }
+  }
+
+  function endAuction() public atStage(Stage.DutchAuction) {
+    // All tiles must be sold to proceed!
+    for (uint8 tileId = minTileId; tileId <= maxTileId; tileId++) {
+      if (tileToOwner[tileId] == address(0)) {
+        require(false);
+      }
+    }
+
+    stage = Stage.GameRounds;
+    _startGameRound();
+  }
+
+  function _startAuction() private {
+    auctionStartTime = now;
+  }
+
+  ///////
+  // Game
+  ///////
 
   uint256 constant public roundDuration = 24 hours;
 
@@ -25,29 +118,24 @@ contract Microverse is HexBoard2, PullPayment, Ownable, TaxRules, JackpotRules {
   uint256 public jackpot;
   uint256 public nextJackpot;
 
-  function roundTimeRemaining() public view returns (uint256) {
-    if (block.timestamp <= roundStartTime.add(roundDuration)) {
+  function roundTimeRemaining() public view atStage(Stage.GameRounds) returns (uint256)  {
+    if (now <= roundStartTime.add(roundDuration)) {
       return 0;
     } else {
-      return (roundStartTime.add(roundDuration)).sub(block.timestamp);
+      return (roundStartTime.add(roundDuration)).sub(now);
     }
   }
 
-  // TODO: Figure out the starting conditions. Maybe we manually start the first round with some jackpot?
-  constructor() public {
-    _startRound();
-  }
-
   // NOTE: Currently callable by anyone
-  function endRound() public {
-    require(block.timestamp >= roundStartTime.add(roundDuration));
+  function endGameRound() public atStage(Stage.GameRounds) {
+    require(now >= roundStartTime.add(roundDuration));
 
     _distributeJackpot();
 
-    _startRound();
+    _startGameRound();
   }
 
-  function setPrice(uint8 tileId, uint256 newPrice) public payable {
+  function setTilePrice(uint8 tileId, uint256 newPrice) public payable atStage(Stage.GameRounds) {
     // must be owner
     require(tileToOwner[tileId] == msg.sender);
 
@@ -61,7 +149,7 @@ contract Microverse is HexBoard2, PullPayment, Ownable, TaxRules, JackpotRules {
     _changeTilePrice(tileId, newPrice);
   }
 
-  function buy(uint8 tileId, uint256 newPrice) public payable {
+  function buyTile(uint8 tileId, uint256 newPrice) public payable atStage(Stage.GameRounds) {
     // can't buy from self
     require(tileToOwner[tileId] != msg.sender);
 
@@ -79,20 +167,14 @@ contract Microverse is HexBoard2, PullPayment, Ownable, TaxRules, JackpotRules {
     _changeTilePrice(tileId, newPrice);
   }
 
-  function _changeTilePrice(uint8 tileId, uint256 newPrice) private {
-    uint256 oldPrice = tileToPrice[tileId];
-    tileToPrice[tileId] = newPrice;
-    totalTileValue = totalTileValue.add(newPrice.sub(oldPrice));
-  }
-
-  function _startRound() private {
+  function _startGameRound() private {
     jackpot = nextJackpot;
     nextJackpot = 0;
-    roundStartTime = block.timestamp;
+    roundStartTime = now;
   }
 
   function _roundOver() private view returns (bool) {
-    return block.timestamp >= roundStartTime.add(roundDuration);
+    return now >= roundStartTime.add(roundDuration);
   }
 
   function _distributeJackpot() private {
@@ -210,10 +292,5 @@ contract Microverse is HexBoard2, PullPayment, Ownable, TaxRules, JackpotRules {
         asyncSend(tileToOwner[tile], allocation);
       }
     }
-  }
-
-  // TODO: Change this once we define 'team'
-  function _sendToTeam(uint256 amount) private {
-    asyncSend(owner, amount);
   }
 }
