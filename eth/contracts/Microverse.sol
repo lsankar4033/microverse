55e8cb1a67e5bfb84863771e383b48dbdef597a8
lsankar4033 <lsankar4033@gmail.com>
2018-10-20 17:02:31 -0700

    Switch to simpler jackpot condition

--- eth/contracts/Microverse.sol
+++ eth/contracts/Microverse.sol
@@ -184,95 +184,42 @@ contract Microverse is
     function _distributeJackpot() private {
         uint256 winnerJackpot = _winnerJackpot(jackpot);
         uint256 landholderJackpot = _landholderJackpot(jackpot);
-        bool distributedJackpot = _distributeWinnerAndLandholderJackpot(winnerJackpot, landholderJackpot);
+        _distributeWinnerAndLandholderJackpot(winnerJackpot, landholderJackpot);
 
         _sendToTeam(_teamJackpot(jackpot));
 
         nextJackpot = nextJackpot.add(_nextPotJackpot(jackpot));
-
-        if (!distributedJackpot) {
-            nextJackpot = nextJackpot.add(winnerJackpot).add(landholderJackpot);
-        }
     }
 
-    // TODO: This can get expensive (although maxes out to O(num_tiles) ! Two approaches to test against
-    // eachother:
-    // 1. iterate once to get all differentials in a memory array, then iterate through that array to determine
-    // how landholder payouts (this is what's implemented now)
-    // 2. iterate through all tiles twice. once to get winner, once to get neighbor differentials
-    //
-    // Returns false if there were no positive differentials this round
-    function _distributeWinnerAndLandholderJackpot(uint256 winnerJackpot, uint256 landholderJackpot) private returns (bool) {
-        uint256[] memory differentials = new uint256[](numTiles + 1); // inc necessary b/c tiles are 1-indexed
-        uint256 numValidDifferentials = 0;
+    function _distributeWinnerAndLandholderJackpot(uint256 winnerJackpot, uint256 landholderJackpot) private {
+        uint256[] memory complements = new uint256[](numTiles + 1); // inc necessary b/c tiles are 1-indexed
+        uint256 totalPriceComplement = 0;
 
-        uint256 bestDifferential = 0;
-        uint8 bestTile = 0;
-
+        uint256 bestComplement = 0;
+        uint8 winningTile = 0;
         for (uint8 i = minTileId; i <= maxTileId; i++) {
-            // NOTE: Should it be possible to have '0' priced land at round's end? Begging for da bots
-            if (tileToOwner[i] != address(0)) {
-                uint256 differential = determineNeighborDifferential(i);
-                differentials[i] = differential;
-                if (differential > 0) {
-                    numValidDifferentials++;
-                }
+            uint256 priceComplement = totalTileValue.sub(tileToPrice[i]);
 
-                if (differential > bestDifferential) {
-                    bestDifferential = differential;
-                    bestTile = i;
-                }
+            // update winner
+            if (bestComplement == 0 || priceComplement > bestComplement) {
+                bestComplement = priceComplement;
+                winningTile = i;
             }
+
+            complements[i] = priceComplement;
+            totalPriceComplement += priceComplement;
         }
 
-        // If no winners, return false so that jackpot is xferred to next round
-        if (numValidDifferentials == 0) {
-            return false;
+        // distribute jackpot
+        asyncSend(tileToOwner[winningTile], winnerJackpot);
+
+        // distribute landholder things
+        for (i = minTileId; i <= maxTileId; i++) {
+            // NOTE: We don't exclude the jackpot winner here, so the winner is paid 'twice'
+            uint256 landholderAllocation = complements[i].mul(landholderJackpot).div(totalPriceComplement);
+
+            asyncSend(tileToOwner[i], landholderAllocation);
         }
-
-        // winner
-        asyncSend(tileToOwner[bestTile], winnerJackpot);
-
-        // all landholders
-        for (uint8 j = minTileId; j <= maxTileId; j++) {
-            if (differentials[j] > 0) {
-                uint256 allocation = landholderJackpot.mul(differentials[j]).div(numValidDifferentials);
-                asyncSend(tileToOwner[j], allocation);
-            }
-        }
-
-        return true;
-    }
-
-    // The key jackpot function.
-    // Currently, only has a nonzero value if *all* neighbors have a greater value, in which case the *average*
-    // price differential is returned
-    function determineNeighborDifferential(uint8 tileId) public view returns (uint256) {
-        uint8[6] memory neighbors = tileToNeighbors[tileId];
-
-        uint256 numValidNeighbors = 0;
-        uint256 totalNeighborDiff = 0;
-
-        for (uint8 i = 0; i < 6; i++) {
-            uint8 neighbor = neighbors[i];
-
-            if (neighbor != nullNeighborValue && tileToOwner[neighbor] != address(0)) {
-                if (tileToPrice[neighbor] <= tileToPrice[tileId]) {
-                    return 0;
-                }
-
-                // NOTE: This only works b/c of preceding check. If that check changes, this must change too
-                uint256 neighborDiff = tileToPrice[neighbor].sub(tileToPrice[tileId]);
-                totalNeighborDiff = totalNeighborDiff.add(neighborDiff);
-                numValidNeighbors++;
-            }
-        }
-
-        if (numValidNeighbors == 0) {
-            return 0;
-        }
-
-        return totalNeighborDiff.div(numValidNeighbors);
     }
 
     // NOTE: Currently haven't implemented sidepot or 'referrals'
