07d9b7918e83850fccc8cd96c8aca3fc2e80d83b
lsankar4033 <lsankar4033@gmail.com>
2018-10-21 17:11:58 -0700

    Remove Migrations.sol

---
+++ eth/scripts/migrate_artifacts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# MUST BE CALLED FROM eth
+cp ./build/contracts/Microverse.json ../web/src/Microverse.json
--- web/scripts/migrate_artifacts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# MUST BE CALLED FROM web
-cp ../eth/build/contracts/Microverse.json ./src/Microverse.json
