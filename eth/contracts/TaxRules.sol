ada6f0a3131b1a3c9a8c8e270d7e7ed398ba5b4a
lsankar4033 <lsankar4033@gmail.com>
2018-10-23 17:54:01 -0700

    Add referrer tax

--- eth/contracts/TaxRules.sol
+++ eth/contracts/TaxRules.sol
@@ -24,12 +24,25 @@ contract TaxRules {
         return (tax.mul(2)).div(5);
     }
 
-    // 5%
-    function _teamTax(uint256 tax) public pure returns (uint256) {
-        return tax.div(20);
+    // 7.5%/5%
+    function _teamTax(uint256 tax, bool hasReferrer) public pure returns (uint256) {
+        if (hasReferrer) {
+            return tax.div(20);
+        } else {
+            return (tax.mul(15)).div(200);
+        }
     }
 
-    // 5%
+    // 2.5% although only invoked if _teamTax is lower value
+    function _referrerTax(uint256 tax, bool hasReferrer)  public pure returns (uint256) {
+        if (hasReferrer) {
+            return tax.div(40);
+        } else {
+            return 0;
+        }
+    }
+
+    // 2.5%
     function _nextPotTax(uint256 tax) public pure returns (uint256) {
         return tax.div(20);
     }
