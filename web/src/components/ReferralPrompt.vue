85add5bdf3a050cb005a5ccb9c5f755f682f52f8
lsankar4033 <lsankar4033@gmail.com>
2018-10-26 16:28:31 -0700

    Copy to clipboard button

--- web/src/components/ReferralPrompt.vue
+++ web/src/components/ReferralPrompt.vue
@@ -2,10 +2,9 @@
     <div id="referral-prompt" class="section section-accent">
         <p>Refer friends and earn 10% of what they spend. No fee to them.</p>
         <p>
-        <input id="referral-url" readonly="readonly" v-bind:value="referralUrl()"/>
+            <input id="referral-url" readonly="readonly" v-bind:value="referralUrl()"/>
 
-            <!-- TODO: Make this button work -->
-            <span class="button inline">
+            <span @click="copyReferralToClipboard" class="button inline">
                 <i class="fas fa-link"></i>Copy link
             </span>
         </p>
@@ -15,8 +14,24 @@
 <script>
 import { mapGetters } from 'vuex'
 
-// NOTE: Probably should put this in a utils file
+// NOTE: Probably should move these to utils file
 const microverseUrl = 'microverse.com'
+function copyStringToClipboard (str) {
+   // Create new element
+   var el = document.createElement('textarea');
+   // Set value (string to be copied)
+   el.value = str;
+   // Set non-editable to avoid focus and move outside of view
+   el.setAttribute('readonly', '');
+   el.style = {position: 'absolute', left: '-9999px'};
+   document.body.appendChild(el);
+   // Select text inside element
+   el.select();
+   // Copy text to clipboard
+   document.execCommand('copy');
+   // Remove temporary element
+   document.body.removeChild(el);
+}
 
 export default{
     name: 'ReferralPrompt',
@@ -27,6 +42,9 @@ export default{
     methods: {
         referralUrl() {
             return `${microverseUrl}?ref=${this.address}`
+        },
+        copyReferralToClipboard() {
+            copyStringToClipboard(this.referralUrl())
         }
     }
 }
