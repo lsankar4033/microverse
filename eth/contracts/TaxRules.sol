e8cbac7a680e8a3e95d6c3fa5af9b939e2a3d97f
lsankar4033 <lsankar4033@gmail.com>
2018-10-30 19:36:21 -0700

    New tax rules

--- eth/contracts/TaxRules.sol
+++ eth/contracts/TaxRules.sol
@@ -14,35 +14,35 @@ contract TaxRules {
 
     // NOTE: The next methods *must* add up to 100%
 
-    // 50%
-    function _jackpotTax(uint256 tax) public pure returns (uint256) {
-        return tax.div(2);
-    }
-
     // 40%
-    function _totalLandholderTax(uint256 tax) public pure returns (uint256) {
+    function _jackpotTax(uint256 tax) public pure returns (uint256) {
         return (tax.mul(2)).div(5);
     }
 
-    // 7.5%/5%
+    // 38%
+    function _totalLandholderTax(uint256 tax) public pure returns (uint256) {
+        return (tax.mul(19)).div(50);
+    }
+
+    // 17%/12%
     function _teamTax(uint256 tax, bool hasReferrer) public pure returns (uint256) {
         if (hasReferrer) {
-            return tax.div(20);
+            return (tax.mul(3)).div(25);
         } else {
-            return (tax.mul(15)).div(200);
+            return (tax.mul(17)).div(100);
         }
     }
 
-    // 2.5% although only invoked if _teamTax is lower value
+    // 5% although only invoked if _teamTax is lower value
     function _referrerTax(uint256 tax, bool hasReferrer)  public pure returns (uint256) {
         if (hasReferrer) {
-            return tax.div(40);
+            return tax.div(20);
         } else {
             return 0;
         }
     }
 
-    // 2.5%
+    // 5%
     function _nextPotTax(uint256 tax) public pure returns (uint256) {
         return tax.div(20);
     }
